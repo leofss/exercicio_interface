8eaadde1f76cbf4e39b65a93df3ddeacb02f32d8
leonardo fernandes <leofernandess11@gmail.com>
2022-03-16 15:39:39 -0300

    Criação das entidades

---
+++ Entities/Installment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_interface.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+    }
+}
